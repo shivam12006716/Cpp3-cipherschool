feece41c23b91a847ff75248473220cf455455e3
Shivam <shivamrajshivam58@gmail.com>
2023-01-27 11:33:32 +0530

    week cpp3

---
+++ C++/pattern4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+  int n;
+  cin >> n;
+  int i = 1;
+  int value = i;
+  while (i <= n)
+    {
+      int j = 1;
+      while (j <= i)
+	{
+	  cout <<value<<" ";
+	   value = value+1;
+
+      j=j+1;
+	}
+	cout<<endl;
+	  i = i + 1;
+    }
+}
+
+
